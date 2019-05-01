5ace5aa2effc81eb866aeff3d76aabcd2187794f
Radu Rentea <radu.rentea@gmail.com>
2019-05-01 11:13:33 +0300

    Add lower and upper limit functionality

--- scripts.js
+++ scripts.js
@@ -1,27 +1,36 @@
-var counter;
-var input;
-var numberInput;
-
-input = prompt("How many random numbers do you want?");
-numberInput = Number(input);
-counter = 0;
-
-//Creates a random number with an upper limit
-function randomNumber(upper)
+//Creates a random number with a lower and upper limit
+function randomNumber(upper, lower)
 {
-    return Math.floor(Math.random() * upper) + 1;
+    return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
 
+var counter;
+var input;
+var input2;
+var input3;
+var numberInput;
+var upperLimit;
+var lowerLimit;
+
+input = prompt("How many random numbers do you want?");
+input2 = prompt("What's the lower limit of the numbers?");
+input3 = prompt("What's the upper limit of the numbers?");
+
+numberInput = Number(input);
+lowerLimit = Number(input2);
+upperLimit = Number(input3);
+counter = 0;
+
 //verifies if the input is a number
-if (isNaN(numberInput))
+if (isNaN(numberInput) || isNaN(lowerLimit) || isNaN(upperLimit))
 {
-    alert("Refresh the page and enter a number");
+    alert("You need to enter only numbers in order to make it work. Please refresh the page and try again!");
 
 }
 //display the numbers on the page
 while (counter < numberInput)
 {
-    var randNum = randomNumber(6);
+    var randNum = randomNumber(upperLimit, lowerLimit);
     document.write(randNum + ' ');
     counter += 1;
 }
