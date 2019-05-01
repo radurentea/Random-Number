d8763570813576d78cbe46570b9a72e218a37ab5
Radu Rentea <radu.rentea@gmail.com>
2019-05-01 10:50:59 +0300

    Add prompt to ask how many numbers the user wants

--- scripts.js
+++ scripts.js
@@ -1,13 +1,32 @@
+var counter;
+var input;
+var numberInput;
+
+input = prompt("How many random numbers do you want?");
+numberInput = Number(input);
+counter = 0;
+
+//Creates a random number with an upper limit
 function randomNumber(upper)
 {
     return Math.floor(Math.random() * upper) + 1;
 }
 
-var counter = 0;
+//verifies if the input is a number
+if (isNaN(numberInput))
+{
+    alert("Refresh the page and enter a number");
 
-while (counter < 10)
+}
+//display the numbers on the page
+while (counter < numberInput)
 {
     var randNum = randomNumber(6);
     document.write(randNum + ' ');
     counter += 1;
 }
+
+
+
+
+
